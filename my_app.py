6c042965cf13c74a2a74b5a570e45be9ab055d08
utakbhate0943-dot <utakbhate0943@sdsu.edu>
2025-10-01 20:45:50 -0700

    Streamlit first draft added

---
+++ my_app.py
@@ -0,0 +1,9 @@
+    # my_app.py
+    import streamlit as st
+
+    st.title(My First Streamlit App)
+    st.write(Hello, Streamlit!)
+
+    name = st.text_input(Enter your name)
+    if name
+        st.write(fWelcome, {name}!)
