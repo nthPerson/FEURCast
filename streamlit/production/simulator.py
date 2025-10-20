42e0243838023ceff8d3b9792e47c13d72033a0b
robert <robertashe2.0@gmail.com>
2025-10-20 00:40:32 -0700

    Remove debug statement that exposes API key (whoops!)

--- streamlit/production/simulator.py
+++ streamlit/production/simulator.py
@@ -21,7 +21,6 @@ def get_openai_client():
     from dotenv import load_dotenv
     load_dotenv(os.path.join(os.path.dirname(__file__), '../..', '.env'))
     api_key = os.getenv('OPENAI_API_KEY')
-    print(api_key)
     return OpenAI(api_key=api_key)
 
 
